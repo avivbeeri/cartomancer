9fd1f6acbdbe9cd80fbbdb5eff477dfb7048793f
Aviv Beeri <avbeeri@gmail.com>
2021-03-16 17:10:25 +0000

    Tidying up

--- nest.sh
+++ nest.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-dome-nest -zo game.egg -- *.wren core entity res scene utils config.json tileRules.json
+dome-nest -zo game.egg -- *.wren core entity res/font res/img scene utils config.json tileRules.json
+cp game.egg ~/Downloads/dome-builds/cartomancer
